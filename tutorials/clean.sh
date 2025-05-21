56ddf6cc7739c2ba91c5b729514535430b27847d
Eli Holmes <eli.holmes@noaa.gov>
2025-05-21 19:06:07 +0000

    clean up misspellings

--- tutorials/clean.sh
+++ tutorials/clean.sh
@@ -1,3 +1,4 @@
+# run with ./clean.sh from tutorials dir
 for nb in ./*.ipynb; do
   jupyter nbconvert --ClearOutputPreprocessor.enabled=True \
     --to notebook --inplace --output "clean/$(basename "$nb")" "$nb"
